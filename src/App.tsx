60b9feea5317294578c1ad3744fb366ec8dbd4a6
tech-souvik <officialsouvikmaji@gmail.com>
2024-01-31 13:41:50 +0530

    Error page and path format change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import "./globals.css";
 import AllDocs from "./components/alldocs/AllDocs";
+import ErrorPage from "./components/common/ErrorPage";
 
 const App = () => {
   return (
-    <main className="flex h-screen">
-      <Routes>
-        <Route path="/" element={<AllDocs />} />
-      </Routes>
+    <main className="flex h-screen !overflow-y-scroll">
+      <Outlet />
     </main>
   );
 };
+
+export const routerPage = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <AllDocs />,
+      },
+    ],
+  },
+  // {
+  //   path: "*",
+  //   element: <ErrorPage />,
+  // },
+]);
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { routerPage } from "./App";
+import { RouterProvider } from "react-router-dom";
 import Provider from "./components/common/Provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <React.StrictMode>
+      <RouterProvider router={routerPage} />
+    </React.StrictMode>
   </Provider>
 );
